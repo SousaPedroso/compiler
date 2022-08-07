d7c7bb3dae94a8977e469d973c911360159beac3
SousaPedroso <gsousa844@gmail.com>
2022-08-07 09:05:18 -0400

    Update. Treatment with commentary characters

--- lexer/Lexer.py
+++ lexer/Lexer.py
@@ -1,4 +1,5 @@
 from .Token import *
+from .InvalidTokenError import InvalidTokenError
 
 digits = set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 
@@ -7,11 +8,11 @@ class Lexer:
     # 'r' is for return and 's' is for shift to another state
     # All identifications of final states of the automata can be replaced
     # by a return here, since we are not moving forward after this state
-    automata = {0: {'{': ['r', OPEN_COMMENT], '}': ['r', CLOSE_COMMENT], '+': ['r', SUM], '-': ['r', SUB], ',': ['r', COMMA],
-                ';': ['r', SEMICOLON], '.': ['r', LAST_POINT], '(': ['r', LP], ')': ['r', RP], 'p': ['s', 2], 'b': ['s', 14],
-                'r': ['s', 22], 'i': ['s', 28], 'w': ['s', 40], '0': ['s', 48], '1': ['s', 48],'2': ['s', 48], '3': ['s', 48],
-                '4': ['s', 48], '5': ['s', 48], '6': ['s', 48], '7': ['s', 48], '8': ['s', 48], '9': ['s', 48], ":": ['s', 54],
-                '*': ['s', 58]},
+    automata = {0: {'+': ['r', SUM], '-': ['r', SUB], ',': ['r', COMMA], ';': ['r', SEMICOLON], '.': ['r', LAST_POINT],
+                '(': ['r', LP], ')': ['r', RP], 'p': ['s', 2], 'b': ['s', 14], 'r': ['s', 22], 'i': ['s', 28], 'w': ['s', 40],
+                '0': ['s', 48], '1': ['s', 48],'2': ['s', 48], '3': ['s', 48], '4': ['s', 48], '5': ['s', 48], '6': ['s', 48],
+                '7': ['s', 48], '8': ['s', 48], '9': ['s', 48], "/": ['s', 54], ":": ['s', 58],
+                '{': ['s', 60], '*': ['s', 62], '}': ['s', 64]},
                 2: {'r': ['s', 4]}, 4: {'o': ['s', 6]}, 6: {'g': ['s', 8]}, 8: {'r': ['s', 10]}, 10: {'a': ['s', 12]}, 12: {'m': ['r', PROGRAM]}, # program
                 14: {'e': ['s', 16]}, 16: {'g': ['s', 18]}, 18: {'i': ['s', 20]}, 20: {'n': ['r', BEGIN]}, # begin
                 22: {'e': ['s', 24]}, 24: {'a': ['s', 26]}, 26: {'l': ['r', REAL_TYPE], 'd': ['r', READ]}, # Read or real
@@ -19,10 +20,10 @@ class Lexer:
                 40: {'r': ['s', 42]}, 42: {'i': ['s', 44]}, 44: {'t': ['s', 46]}, 46: {'e': ['r', WRITE]}, # write
                 48: {'.': ['s', 50]}, # float number
                 50: {'0': ['s', 52], '1': ['s', 52],'2': ['s', 52], '3': ['s', 52], '4': ['s', 52], '5': ['s', 52], '6': ['s', 52], '7': ['s', 52], '8': ['s', 52], '9': ['s', 52]},
-                54: {'*': ['r', SLASH_OPEN_COMMENT]},
-                56: {'=': ['r', ASSIGN]},
-                58: {'/': ['r', SLASH_CLOSE_COMMENT]}
+                54: {'*': ['s', 56]},
+                58: {'=': ['r', ASSIGN]},
                 }
+    
     line = 1
     stop_pos = 0
     ignore = True # Lines of commentary
@@ -30,8 +31,9 @@ class Lexer:
     def __init__(self, source_code):
         with open(source_code, "r") as file:
             self.source_code = file.read()
-            self.state = 0
             self.tam = len(self.source_code)
+            self.source_code += "$"
+            self.state = 0
 
     def get_token(self):
         state = 0
@@ -45,30 +47,64 @@ class Lexer:
             elif self.source_code[cur_pos] == '\n':
                 self.line += 1
 
-            # Check presence of commentaries
             else:
                 break
 
-        next_action = self.automata[state][self.source_code[cur_pos]]
+        next_action = self.automata[state].get(self.source_code[cur_pos])
+        if next_action == None:
+            raise InvalidTokenError(f"Invalid token {self.state[cur_pos]} at line {self.line}")
 
-        if next_action[0] == 's':
+        elif next_action[0] == 's':
             state = next_action[1]
-            # Since we are representing as key-value the automata,
-            # some atention is needed in some states
+            self.stop_pos = cur_pos + 1
 
-        else:
-            # check commentaries
-            if next_action[1] == OPEN_COMMENT:
-                self.ignore = True
+            # Get next action to check possible commentary with /*
+            if cur_pos+1 != self.tam: # last token
+                next_action = self.automata[state].get(self.source_code[cur_pos+1])
 
-            elif next_action[1] == CLOSE_COMMENT:
-                self.ignore = False
-
-            elif not(self.ignore):
-                return next_action[1]
-
-            # Check possibility for other comen
-            elif self.stop_pos == self.tam:
-                return EOS()
+            elif cur_pos+1 == self.tam:
+                raise InvalidTokenError(f"Invalid token {self.state[cur_pos]} at line {self.line}")
             
-# Arrumar o desenho automato do léxico para ler digitos enquanto houver
+            elif next_action[0] == 'r':
+                return next_action[1]()
+
+            # Check next state whether is a *
+            next_state = next_action[1]
+
+            # Begining of comentary
+            if state == 60 or next_state == 56:
+                if next_state == 56:
+                    self.stop_pos += 1
+                    state = 56
+
+
+                # Ignore other tokens
+                for cur_pos in range(self.stop_pos, self.tam):
+                    if (state == 56 and self.automata[cur_pos] != '*') or (self.state == 60 and self.automata[cur_pos] != '}'):
+                        continue
+
+                    # First condition of commentary
+                    elif self.state == 60 and self.automata[cur_pos] != '}':
+                        break
+
+                    # Second condition of commentary
+                    elif (state == 56 and self.automata[cur_pos] == '*'):
+                        if cur_pos+1 != self.tam and self.automata[cur_pos+1] == '/':
+                            break
+
+                self.stop_pos = cur_pos + 1
+
+            elif self.stop_pos != self.tam:
+                # Continue reading until a token
+                pass
+
+            else:
+                return EOS()
+
+        elif self.stop_pos != self.tam:
+            self.stop_pos = cur_pos +1
+            return next_action[1]()
+
+        elif self.stop_pos == self.tam:
+            return EOS()
+            
