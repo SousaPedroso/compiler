918d35b9dc86b82e72b4f56fb5c4d3d0e4a4a6bf
SousaPedroso <gsousa844@gmail.com>
2022-08-09 12:56:57 -0400

    Test of Lexer

--- main/Main.py
+++ main/Main.py
@@ -1,13 +1,17 @@
 import os
 from lexer.Lexer import Lexer
-from lexer.Tag import *
+from lexer.Token import *
 
 def main():
     tests_dir = os.path.abspath("..\\tests")
 
     lexer = Lexer(os.path.join(tests_dir, "ex01"))
     token = lexer.get_token()
-    inputs = [[token]]
-    last_line = lexer.line
-    # Recover the values
-    entradas = [[token.value]]
+    input = [token]
+    while token.tag != Tag.EOS:
+        print(token, token.value)
+        token = lexer.get_token()
+        input.append(token)
+
+if __name__ == "__main__":
+    main()
