79493b650123f4001bdd1e6b846103c503a3de58
SousaPedroso <gsousa844@gmail.com>
2022-08-13 20:53:31 -0400

    Removed rules which do not generate pseudocode

--- semantic/Semantic.py
+++ semantic/Semantic.py
@@ -5,41 +5,31 @@ class Semantic:
 
     # Types for each rule
     # T: Type for a variable
-    # DT: Delete type for a variable
     # U: Unary operator (-1)
     # AS: Assignment for a identifier
     # R: Input
     # W: print
-    # EX: expression (POP VALUES FROM TWO STACKS AND COMPUTES THE VALUE)
-    # SO: SumOperation (the operation to be applied)
+    # EX: expression (Generate the operation to be computed)
+    # SO: SumOperation
     # SUO: SUbtrationOperation
     # NI: NumeroInteiro
     # NR: NumeroReal
-    # TE: Termo
     # MO: MultiplicationOperation
     # DO: DivisionOperation
     # EA: ExpressionAttribution
-    # MF: MaisFatores
     # ID: IDent
-    # PU: ProductUnit
-    # SU: SumUnit
 
 
     # Defines how each non terminal will be updated according to the operation
-    operations = {"T": lambda T, V: T.update({V: ''}), "DT": "", "AS": lambda N: N.pop(),
+    operations = {"T": lambda T, V: T.update({V: ''}), "AS": lambda N: N.pop(),
     "U": lambda V: -1*V, "R": input, "W": lambda V: print(V), "EX": lambda OP1, OP2: (OP1.pop(), OP2.pop()),
-    "SO": "sum", "SUO": "sub", "NI": lambda N: N, "NR": lambda N: N, "TE": lambda FA, MF: (FA.pop(), MF.pop()),
-    "MO": "mul", "DO": "div", "EA": lambda E: E.pop(), "MF": lambda FA, MF: (FA.pop(), MF.pop()),
-    "OT": lambda FA, MF: (FA.pop(), MF.pop()), "ID": lambda I: I, "PU": "",
-    "SU": lambda OT: OT.append(0)}
+    "NI": lambda N: N, "NR": lambda N: N, "ID": lambda I: I, "MO": "mul", "DO": "div", "SO": "sum", "SUO": "sub"}
 
-    # Stores the rules to each state to recover the value for each operation
+    # Stores the rules to generate the pseudocode
     rules = {
         8: {":": "T"},
         9: {":": "T"},
-        20: {"begin": "DT", ";": "DT"},
         33: {"end": "AS", ";": "AS"},
-        34: {"end": "SU", ";": "SU", ")": "SU"},
         36: {"ident": "U", "(": "U", "numero_int": "U", "numero_real": "U"},
         38: {"end": "R", ";": "R"},
         39: {"end": "W", ";": "W"},
@@ -48,15 +38,8 @@ class Semantic:
         43: {"ident": "SUO", "end": "SUO", ";": "SUO", "(": "SUO", "+": "SUO", "-": "SUO", "numero_int": "SUO", "numero_real": "SUO"},
         44: {"end": "NI", ";": "NI", "*": "NI", "/": "NI", "+": "NI", "-": "NI"},
         45: {"end": "NR", ";": "NR", "*": "NR", "/": "NR", "+": "NR", "-": "NR"},
-        47: {"end": "SU", ";": "SU", ")": "SU"},
-        48: {"end": "TE", ";": "TE", ")": "TE", "+": "TE", "-": "TE"},
         50: {"ident": "MO", "(": "MO", "numero_int": "MO", "numero_real": "MO"},
         51: {"ident": "DO", "(": "DO", "numero_int": "DO", "numero_real": "DO"},
-        53: {"end": "PU", ";": "PU", "+": "PU", "-": "PU"},
-        54: {"end": "EA", ";": "EA", "*": "EA", "/": "EA", "+": "EA", "-": "EA"},
-        55: {"end": "MF", ";": "MF", "+": "MF", "-": "MF"},
-        56: {"end": "OT", ";": "OT", ")": "OT"},
-        58: {"end": "PU", ";": "PU", "+": "PU", "-": "PU"},
         59: {"end": "ID", ";": "ID", "*": "ID", "/": "ID", "+": "ID", "-": "ID"}
     }
 
